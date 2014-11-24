b8e30e5c1403400bc74fffab2560cd05af0cc530
matejbasic <mbasic0@foi.hr>
2014-11-24 12:23:31 +0100

    fb user

---
+++ MusicMap/MusicMap/src/net/neurolab/musicmap/presenters/UserPresenter.java
@@ -0,0 +1,9 @@
+package net.neurolab.musicmap.presenters;
+
+import net.neurolab.musicmap.db.User;
+
+public class UserPresenter {
+	private User user;
+	
+	
+}
---
+++ MusicMap/MusicMap/src/net/neurolab/musicmap/ws/MM.java
@@ -0,0 +1,26 @@
+package net.neurolab.musicmap.ws;
+
+import android.os.AsyncTask;
+
+public class MM extends AsyncTask<Object, Void, Object[]> {
+	
+	private String URL = "http://musicmap.cloudapp.net/api";
+	private String appKey = "2c9s1rwf7578307";
+	
+	public String registerFacebookUser(String facebookId) {
+		
+		return "";
+	}
+	
+	private String passwordGenerator() {
+		
+		return "x";
+	}
+	
+	@Override
+	protected Object[] doInBackground(Object... params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
